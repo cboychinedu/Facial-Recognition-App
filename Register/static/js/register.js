ebc78d21ace2a636993679bdf104ec93e1cc4079
Mbonu Chinedum <cboy.chinedu@gmail.com>
2025-02-02 17:42:25 +0100

    working on the register.js ile

---
+++ Register/static/js/register.js
@@ -0,0 +1,16 @@
+// Debug 
+console.log("Made by Mbonu Chinedum")
+
+// Getting the dom elements 
+const firstname = document.getElementById("firstname"); 
+const lastname = document.getElementById("lastname"); 
+const emailAddress = document.getElementById("emailAddress"); 
+const password = document.getElementById("password"); 
+const registerBtn = document.getElementById("registerBtn"); 
+
+// Setting an event listener for the register button 
+registerBtn.addEventListener("click", (event) => {
+    event.preventDefault(); 
+
+    
+})
